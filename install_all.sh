5f7eea43b6d11cb7818027af0344bdca97182ee8
t-o-m-s-c-o-t-t <tomscott87@gmail.com>
2016-05-07 21:17:23 +0100

    Removed vim addon lines since migrating to vundle.

--- install_all.sh
+++ install_all.sh
@@ -27,22 +27,5 @@ mkdir -p ~/.vim/colors && \
 cd ~/.vim/colors && \
 wget https://raw.githubusercontent.com/sjl/badwolf/master/colors/badwolf.vim
 
-# VIM ADDONS
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-# Syntastic
-cd ~/.vim/bundle && \
-git clone https://github.com/scrooloose/syntastic.git
-
-# NERD tree
-cd ~/.vim/bundle && \
-git clone https://github.com/scrooloose/nerdtree.git
-
-# Airline
-cd ~/.vim/bundle && \
-git clone https://github.com/bling/vim-airline
-
-
 # ZSH
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
